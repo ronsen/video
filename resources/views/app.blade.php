058cffa8f9bd7cd70f76a4a8ff21b9bd42cd996c
Ronsen <ronsen@startrek.or.id>
2024-07-26 16:10:11 +0700

    add: icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+	<link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
 	@vite("resources/js/app.js")
 	@inertiaHead
