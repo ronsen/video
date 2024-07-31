ff8e7f99a2db567a729c56a96a3f91f03c0e5bcb
Ronsen <ronsen@startrek.or.id>
2024-07-31 12:52:18 +0700

    fix: auth

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,7 @@ class LoginRequest extends FormRequest
 	{
 		$this->ensureIsNotRateLimited();
 
-		if (!Auth::attempt(array_merge($this->only('email', 'password'), ['active' => true]), $this->boolean('remember'))) {
+		if (!Auth::attempt(array_merge($this->only('email', 'password')), true)) {
 			RateLimiter::hit($this->throttleKey());
 
 			throw ValidationException::withMessages([
