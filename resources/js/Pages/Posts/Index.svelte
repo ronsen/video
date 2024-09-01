258902d623c05d5e4389d21a90a1a284f820e62b
Ronsen <ronsen@startrek.or.id>
2024-09-01 14:52:31 +0700

    use persistent layout

--- resources/js/Pages/Posts/Index.svelte
+++ resources/js/Pages/Posts/Index.svelte
@@ -1,6 +1,9 @@
+<script context="module">
+    export { default as layout } from "../Layouts/App.svelte";
+</script>
+
 <script>
     import { page, Link, useForm } from "@inertiajs/svelte";
-    import App from "../Layouts/App.svelte";
     import Alert from "../Components/Alert.svelte";
     import Pagination from "../Components/Pagination.svelte";
     import Delete from "../Components/Delete.svelte";
@@ -27,46 +30,44 @@
     <title>{$page.props.appName}</title>
 </svelte:head>
 
-<App>
-    <div class="mb-6">
-        <form on:submit|preventDefault={submit}>
-            <div class="join w-full">
-                <input
-                    type="search"
-                    bind:value={$form.q}
-                    class="input input-bordered w-full join-item"
-                    required
-                />
-                <button
-                    type="submit"
-                    disabled={$form.processing}
-                    class="btn btn-primary join-item"
-                    ><Fa icon={faMagnifyingGlass} /></button
-                >
-            </div>
-        </form>
-    </div>
-
-    {#if posts.data.length == 0}
-        <Alert>Empty.</Alert>
-    {:else}
-        {#each posts.data as post}
-            <div
-                class="flex justify-between items-center border-b border-base-300 pb-2 mb-2"
+<div class="mb-6">
+    <form on:submit|preventDefault={submit}>
+        <div class="join w-full">
+            <input
+                type="search"
+                bind:value={$form.q}
+                class="input input-bordered w-full join-item"
+                required
+            />
+            <button
+                type="submit"
+                disabled={$form.processing}
+                class="btn btn-primary join-item"
+                ><Fa icon={faMagnifyingGlass} /></button
             >
-                <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
+        </div>
+    </form>
+</div>
 
-                {#if $page.props.auth.user}
-                    <div class="inline-flex gap-3">
-                        <Link href="/posts/{post.id}/edit" class="text-base-300"
-                            ><Fa icon={faPencilAlt} /></Link
-                        >
-                        <Delete {post} />
-                    </div>
-                {/if}
-            </div>
-        {/each}
+{#if posts.data.length == 0}
+    <Alert>Empty.</Alert>
+{:else}
+    {#each posts.data as post}
+        <div
+            class="flex justify-between items-center border-b border-base-300 pb-2 mb-2"
+        >
+            <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
 
-        <Pagination data={posts} />
-    {/if}
-</App>
+            {#if $page.props.auth.user}
+                <div class="inline-flex gap-3">
+                    <Link href="/posts/{post.id}/edit" class="text-base-300"
+                        ><Fa icon={faPencilAlt} /></Link
+                    >
+                    <Delete {post} />
+                </div>
+            {/if}
+        </div>
+    {/each}
+
+    <Pagination data={posts} />
+{/if}
--- resources/js/Pages/Posts/Show.svelte
+++ resources/js/Pages/Posts/Show.svelte
@@ -1,6 +1,9 @@
+<script context="module">
+    export { default as layout } from "../Layouts/App.svelte";
+</script>
+
 <script>
-    import { page, Link, useForm } from "@inertiajs/svelte";
-    import App from "../Layouts/App.svelte";
+    import { page, Link } from "@inertiajs/svelte";
     import Delete from "../Components/Delete.svelte";
 
     import Fa from "svelte-fa";
@@ -22,40 +25,38 @@
     <title>{post.title}</title>
 </svelte:head>
 
-<App>
-    <article>
-        <div bind:this={video} class="relative mb-3">
-            <img
-                src={post.thumbnail_url}
-                alt={post.title}
-                class="w-full rounded-lg"
-            />
+<article>
+    <div bind:this={video} class="relative mb-3">
+        <img
+            src={post.thumbnail_url}
+            alt={post.title}
+            class="w-full rounded-lg"
+        />
 
-            <button
-                on:click={play}
-                class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-red-500 hover:text-red-600"
-            >
-                <Fa icon={faPlayCircle} size="4x" />
-            </button>
-        </div>
-
-        <div
-            class="flex justify-between items-center border-b border-base-300 pb-3 mb-3"
+        <button
+            on:click={play}
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-red-500 hover:text-red-600"
         >
-            <div class="title font-bold">{post.title}</div>
+            <Fa icon={faPlayCircle} size="4x" />
+        </button>
+    </div>
 
-            {#if $page.props.auth.user}
-                <div class="inline-flex items-center gap-3">
-                    <Link href="/posts/{post.id}/edit" class="text-base-300"
-                        ><Fa icon={faPencilAlt} /></Link
-                    >
-                    <Delete {post} />
-                </div>
-            {/if}
-        </div>
+    <div
+        class="flex justify-between items-center border-b border-base-300 pb-3 mb-3"
+    >
+        <div class="title font-bold">{post.title}</div>
 
-        <div class="content max-w-none mb-3">
-            {@html post.content_to_html}
-        </div>
-    </article>
-</App>
+        {#if $page.props.auth.user}
+            <div class="inline-flex items-center gap-3">
+                <Link href="/posts/{post.id}/edit" class="text-base-300"
+                    ><Fa icon={faPencilAlt} /></Link
+                >
+                <Delete {post} />
+            </div>
+        {/if}
+    </div>
+
+    <div class="content max-w-none mb-3">
+        {@html post.content_to_html}
+    </div>
+</article>
