9b322779a3a5c760d7638dc83dada32e25109f29
Ronsen <ronsen@startrek.or.id>
2024-09-04 13:46:38 +0700

    add: TrustProxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+	/**
+	 * The trusted proxies for this application.
+	 *
+	 * @var string|array
+	 */
+	protected $proxies = '*';
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,22 +1,24 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-		api: __DIR__.'/../routes/api.php',
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
+	->withRouting(
+		api: __DIR__ . '/../routes/api.php',
+		web: __DIR__ . '/../routes/web.php',
+		commands: __DIR__ . '/../routes/console.php',
+		health: '/up',
+	)
+	->withMiddleware(function (Middleware $middleware) {
+		$middleware->web(append: [
 			HandleInertiaRequests::class,
+			TrustProxies::class,
 		]);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+	})
+	->withExceptions(function (Exceptions $exceptions) {
+		//
+	})->create();
