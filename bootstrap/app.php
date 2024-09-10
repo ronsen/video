28f1e4130c7409475b267aeac518b225c9f87bcf
Ronsen <ronsen@startrek.or.id>
2024-09-10 15:43:03 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
-use App\Http\Middleware\TrustProxies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,8 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
 	->withMiddleware(function (Middleware $middleware) {
 		$middleware->web(append: [
 			HandleInertiaRequests::class,
-			TrustProxies::class,
-		]);
+		])
+		->trustProxies(at: '*');
 	})
 	->withExceptions(function (Exceptions $exceptions) {
 		//
