0db1e9f7ed495ec4011b24fc959746970bebaa20
Ronsen <ronsen@startrek.or.id>
2024-11-28 21:26:06 +0700

    refactor: posts

--- resources/js/Pages/Posts/Index.svelte
+++ resources/js/Pages/Posts/Index.svelte
@@ -3,14 +3,9 @@
 </script>
 
 <script>
-    import { page, Link, useForm } from "@inertiajs/svelte";
-    import Alert from "../Components/Alert.svelte";
-    import Delete from "../Components/Delete.svelte";
-    import Pagination from "../Components/Pagination.svelte";
+    import { page } from "@inertiajs/svelte";
+    import Posts from "../Components/Posts.svelte";
     import Search from "../Components/Search.svelte";
-
-    import Fa from "svelte-fa";
-    import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
     let { posts, q } = $props();
 </script>
@@ -23,27 +18,4 @@
 	<Search {q} />
 </section>
 
-{#if posts.data.length == 0}
-    <Alert>Empty.</Alert>
-{:else}
-    {#each posts.data as post}
-        <div
-            class="flex justify-between items-center border-b border-zinc-600 gap-3 pb-2 mb-2"
-        >
-            <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
-
-            {#if $page.props.auth.user}
-                <div class="inline-flex gap-3">
-                    <Link
-                        href="/posts/{post.id}/edit"
-                        class="text-zinc-400 hover:text-zinc-300"
-                        ><Fa icon={faPencilAlt} /></Link
-                    >
-                    <Delete {post} />
-                </div>
-            {/if}
-        </div>
-    {/each}
-
-    <Pagination data={posts} />
-{/if}
+<Posts {posts} />
--- resources/js/Pages/Tags/Show.svelte
+++ resources/js/Pages/Tags/Show.svelte
@@ -3,13 +3,7 @@
 </script>
 
 <script>
-    import { page, Link } from "@inertiajs/svelte";
-    import Alert from "../Components/Alert.svelte";
-    import Pagination from "../Components/Pagination.svelte";
-    import Delete from "../Components/Delete.svelte";
-
-    import Fa from "svelte-fa";
-    import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+	import Posts from "../Components/Posts.svelte";
 
     let { posts, slug } = $props();
 </script>
@@ -22,27 +16,4 @@
     <a href="/tag/{slug}">{slug}</a>
 </h3>
 
-{#if posts.data.length == 0}
-    <Alert>Empty.</Alert>
-{:else}
-    {#each posts.data as post}
-        <div
-            class="flex justify-between items-center border-b border-zinc-600 gap-3 pb-2 mb-2"
-        >
-            <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
-
-            {#if $page.props.auth.user}
-                <div class="inline-flex gap-3">
-                    <Link
-                        href="/posts/{post.id}/edit"
-                        class="text-zinc-400 hover:text-zinc-300"
-                        ><Fa icon={faPencilAlt} /></Link
-                    >
-                    <Delete {post} />
-                </div>
-            {/if}
-        </div>
-    {/each}
-
-    <Pagination data={posts} />
-{/if}
+<Posts {posts} />
