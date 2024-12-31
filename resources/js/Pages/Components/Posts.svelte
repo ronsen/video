c4943574319fdf9a4d6666d5de370edcf5b11a10
Ronsen <ronsen@startrek.or.id>
2024-12-31 13:05:06 +0700

    update: deps

--- resources/js/Pages/Components/Posts.svelte
+++ resources/js/Pages/Components/Posts.svelte
@@ -1,11 +1,7 @@
 <script>
-    import { page, Link } from "@inertiajs/svelte";
+    import { Link } from "@inertiajs/svelte";
     import Alert from "../Components/Alert.svelte";
-    import Delete from "../Components/Delete.svelte";
     import Pagination from "../Components/Pagination.svelte";
-
-    import Fa from "svelte-fa";
-    import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
     let { posts } = $props();
 </script>
@@ -13,24 +9,22 @@
 {#if posts.data.length == 0}
     <Alert>Empty.</Alert>
 {:else}
-    {#each posts.data as post}
-        <div
-            class="flex justify-between items-center border-b border-zinc-600 gap-3 pb-2 mb-2"
-        >
-            <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
-
-            {#if $page.props.auth.user}
-                <div class="inline-flex gap-3">
-                    <Link
-                        href="/posts/{post.id}/edit"
-                        class="text-zinc-400 hover:text-zinc-300"
-                        ><Fa icon={faPencilAlt} /></Link
-                    >
-                    <Delete {post} />
+    <div class="grid md:grid-cols-2 gap-4">
+        {#each posts.data as post}
+            <div>
+                <Link href="/v/{post.id}/{post.slug}"
+                    ><img
+                        src={post.thumbnail_url}
+                        alt={post.title}
+                        class="w-full rounded-lg object-cover"
+                    />
+                </Link>
+                <div class="mt-1">
+                    <Link href="/v/{post.id}/{post.slug}">{post.title}</Link>
                 </div>
-            {/if}
-        </div>
-    {/each}
+            </div>
+        {/each}
+    </div>
 
     <Pagination data={posts} />
 {/if}
