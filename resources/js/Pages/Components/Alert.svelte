97cdb539eeaa7b691bb54504b85577b097832dc3
Ronsen <ronsen@startrek.or.id>
2025-02-28 12:06:28 +0700

    update

--- resources/js/Pages/Components/Alert.svelte
+++ resources/js/Pages/Components/Alert.svelte
@@ -13,7 +13,7 @@
 
 {#if show}
 	<div
-		class="flex justify-between items-center shadow-lg border border-zinc-500 p-4 rounded-lg mb-6"
+		class="flex justify-between items-center shadow-lg border border-zinc-500 p-4 rounded-lg my-6"
 	>
 		<div class="inline-flex items-center gap-2">
 			<Info size={16} />
