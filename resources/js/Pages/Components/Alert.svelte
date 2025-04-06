4dc70b930642a608ec203411a756be9444a7e8e5
Ronsen <ronsen@startrek.or.id>
2025-04-06 12:29:22 +0700

    update

--- resources/js/Pages/Components/Alert.svelte
+++ resources/js/Pages/Components/Alert.svelte
@@ -4,22 +4,17 @@
 	let { children } = $props();
 
 	let show = $state(true);
-
-	function close(e) {
-		e.preventDefault();
-		show = !show;
-	}
 </script>
 
 {#if show}
 	<div
-		class="flex justify-between items-center shadow-lg border border-zinc-500 p-4 rounded-lg my-6"
+		class="flex justify-between items-center shadow-lg border border-zinc-500 p-4 rounded-lg m-6"
 	>
 		<div class="inline-flex items-center gap-2">
 			<Info size={16} />
 			{@render children()}
 		</div>
 		<span></span>
-		<button onclick={close}><X size={16} /></button>
+		<button onclick={show = !show} class="cursor-pointer"><X size={16} /></button>
 	</div>
 {/if}
