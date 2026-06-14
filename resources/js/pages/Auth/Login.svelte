b5332f1670c64d50cd19239067f2ac63174b82cd
Ronsen <ronsen@startrek.or.id>
2026-06-14 10:24:27 +0700

    upgrade: filamentphp

--- resources/js/pages/Auth/Login.svelte
+++ resources/js/pages/Auth/Login.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import { useForm } from "@inertiajs/svelte";
+	import { Form } from "@inertiajs/svelte";
 	import Divider from "@/components/Divider.svelte";
 	import Guest from "@/layouts/Guest.svelte";
 
 	let { title }: { title: string } = $props();
-
-	let form = useForm({
-		email: "",
-		password: "",
-	});
-
-	function submit(e: SubmitEvent) {
-		e.preventDefault();
-		form.post("/login", {
-			onFinish: () => form.reset("password"),
-		});
-	}
 </script>
 
 <svelte:head>
@@ -35,39 +23,41 @@
 				>
 			</div>
 
-			<form onsubmit={submit}>
-				<div class="mb-3">
-					<input
-						type="email"
-						bind:value={form.email}
-						placeholder="E-mail"
-						class="border border-zinc-600 rounded-lg bg-zinc-800 text-white/90 w-full"
-					/>
-					{#if form.errors.email}
-						<div class="text-red-500 text-xs mt-1">
-							{form.errors.email}
-						</div>
-					{/if}
-				</div>
-				<div class="mb-3">
-					<input
-						type="password"
-						bind:value={form.password}
-						placeholder="Password"
-						class="border border-zinc-600 rounded-lg bg-zinc-800 text-white/90 w-full"
-					/>
-					{#if form.errors.password}
-						<div class="text-red-500 text-xs mt-1">
-							{form.errors.password}
-						</div>
-					{/if}
-				</div>
-				<button
-					type="submit"
-					class="p-2 font-semibold border border-zinc-200 rounded-full bg-zinc-200 hover:bg-zinc-100 text-sm text-black/90 w-full"
-					disabled={form.processing}>Log In</button
-				>
-			</form>
+			<Form action="/login" method="post" disableWhileProcessing>
+				{#snippet children({ errors, processing })}
+					<div class="mb-3">
+						<input
+							type="email"
+							name="email"
+							placeholder="E-mail"
+							class="border border-zinc-600 rounded-lg bg-zinc-800 text-white/90 w-full"
+						/>
+						{#if errors.email}
+							<div class="text-red-500 text-xs mt-1">
+								{errors.email}
+							</div>
+						{/if}
+					</div>
+					<div class="mb-3">
+						<input
+							type="password"
+							name="password"
+							placeholder="Password"
+							class="border border-zinc-600 rounded-lg bg-zinc-800 text-white/90 w-full"
+						/>
+						{#if errors.password}
+							<div class="text-red-500 text-xs mt-1">
+								{errors.password}
+							</div>
+						{/if}
+					</div>
+					<button
+						type="submit"
+						class="p-2 font-semibold border border-zinc-200 rounded-full bg-zinc-200 hover:bg-zinc-100 text-sm text-black/90 w-full"
+						disabled={processing}>Log In</button
+					>
+				{/snippet}
+			</Form>
 		</div>
 
 		<Divider text="Sign in or register" />
